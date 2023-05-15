1afff9d9ef73736a534ec41d2b36f75701c36d52
js-ha <jshaha0911@gmail.com>
2023-05-15 18:21:09 +0900

    feat : 이메일, 패스워드 사용 회원가입 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import signUp from './firebase/signUp';
 function App() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleClick = (e, setState) => {
+    setState(e.target.value);
+  };
   return (
     <div>
       <h1 className={`text-[35px] font-bold underline`}>Hello world!</h1>
       <h1 className='test'>Test</h1>
-      <input placeholder='email'></input>
-      <input placeholder='password'></input>
+      <input
+        placeholder='email'
+        onChange={(e) => handleClick(e, setEmail)}
+      ></input>
+      <input
+        placeholder='password'
+        onChange={(e) => handleClick(e, setPassword)}
+      ></input>
       <button onClick={() => signUp(email, password)}>SignUp</button>
     </div>
   );
