f340abb2662334a66fd4c9f36fb57111763c1149
rosenfence <rosenfence@gmail.com>
2023-05-15 18:52:17 +0900

    feat : SignUp component 분리, 회원가입 유효성 검사 추가, 함수명 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import { useState } from 'react';
-import signUp from './firebase/signUp';
+import React from 'react';
+import { SignUp } from './components';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleClick = (e, setState) => {
-    setState(e.target.value);
-  };
   return (
     <div>
       <h1 className={`text-[35px] font-bold underline`}>Hello world!</h1>
       <h1 className='test'>Test</h1>
-      <input
-        placeholder='email'
-        onChange={(e) => handleClick(e, setEmail)}
-      ></input>
-      <input
-        placeholder='password'
-        onChange={(e) => handleClick(e, setPassword)}
-      ></input>
-      <button onClick={() => signUp(email, password)}>SignUp</button>
+      <SignUp />
     </div>
   );
 }
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import signUp from '../firebase/signUp';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const emailRegex = new RegExp('[a-z0-9]+@[a-z]+.[a-z]{2,3}');
+
+  const handleChange = (e, setState) => {
+    setState(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (emailRegex.test(email) === false) {
+      alert('Please enter a valid email');
+    } else if (password.length < 6) {
+      alert('Password must be at least 6 characters');
+    } else {
+      signUp(email, password);
+    }
+  };
+
+  return (
+    <>
+      <input placeholder='email' onChange={(e) => handleChange(e, setEmail)}></input>
+      <input placeholder='more than 4 letters' type='password' onChange={(e) => handleChange(e, setPassword)}></input>
+      <button onClick={handleClick}>SignUp</button>
+    </>
+  );
+};
+export default SignUp;
