07fa9f9a7b933c1e8dcd4b8cf046e0d45bd892d3
rosenfence <rosenfence@gmail.com>
2023-05-23 20:11:19 +0900

    design : Button component style 수정

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Button = ({ onClick, children }) => {
   return (
-    <button className='w-18' onClick={onClick}>
+    <button className='border-2 w-18' onClick={onClick}>
       {children}
     </button>
   );
