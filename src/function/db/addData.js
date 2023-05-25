0ea6183695ced77a3f9bf026a819b8582c27a331
rosenfence <rosenfence@gmail.com>
2023-05-25 22:54:33 +0900

    fix : addData 데이터 추가 방식 변경

--- src/function/db/addData.js
+++ src/function/db/addData.js
@@ -1,15 +1,14 @@
-import { addDoc, collection } from 'firebase/firestore';
+import { doc, setDoc } from 'firebase/firestore';
 import { db } from '../../firebase/firebase';
 
 export default async function addData(id, content, date) {
   try {
-    const docRef = await addDoc(collection(db, 'users'), {
-      id,
+    const docRef = await setDoc(doc(db, 'users', id), {
       content,
       date,
     });
 
-    console.log('Document written with ID: ', docRef.id);
+    console.log('Document written with ID: ', docRef);
   } catch (e) {
     console.error('Error adding document: ', e);
   }
--- src/function/db/getData.js
+++ src/function/db/getData.js
@@ -5,7 +5,6 @@ export default async function getData(setLists) {
   const querySnapshot = await getDocs(collection(db, 'users'));
   let arr = [];
   querySnapshot.forEach((doc) => {
-    // console.log(`${doc.id} => ${doc.data()}`);
     arr.push(doc.data());
   });
   setLists(arr);
