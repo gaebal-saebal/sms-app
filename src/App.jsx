05cca13ae258234365a79bbaa735325564368f18
rosenfence <rosenfence@gmail.com>
2023-05-28 18:07:39 +0900

    chore : app component 수월한 개발을 위한 코드 정리

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,55 @@
 import React, { useState, useEffect } from 'react';
+// import components from componets dir
 import { Button, SignUp } from './components';
+// import handle DB functions from function/db dir
 import { addData, getData, deleteData } from './function/db';
+// import handle Auth functions from function/auth dir
 import { handleGithubLogin, handleGoogleLogin, handleLogOut } from './function/auth';
 
 function App() {
-  const [user, setUser] = useState('로그인 해주세요');
-  const [id, setId] = useState(0);
-  const [content, setContent] = useState('');
-  const [lists, setLists] = useState([]);
+  const [user, setUser] = useState('로그인 해주세요'); // 현재 user(비로그인/로그아웃시 '로그인 해주세요')
+  const [id, setId] = useState(0); // DB에 담길 id
+  const [content, setContent] = useState(''); // DB에 담길 content
+  const [lists, setLists] = useState([]); // DB에서부터 가져온 data list
 
   useEffect(() => {
     getData(setLists);
-  }, []);
+  }, []); // '/' 페이지가 로드될 시 data list 가져옴
 
   return (
     <div>
-      <h1 className={`text-[35px] font-bold underline`}>Hello world!</h1>
-      <h1 className='test'>Test</h1>
-      <SignUp />
-      <p> {user}</p>
+      <header>
+        <h1 className={`text-[35px] font-bold underline`}>Hello world!</h1>
+        <h1 className='test'>Test</h1>
+      </header>
 
-      <Button onClick={() => handleGoogleLogin(setUser)}>Log in with google</Button>
-      <Button onClick={() => handleGithubLogin(setUser)}>Log in with github</Button>
-      <Button onClick={() => handleLogOut(setUser)}>Log-out</Button>
+      <div className='my-10 border-2'>
+        <p> {user}</p>
+        <SignUp />
 
-      <div>
+        {/* Auth buttons */}
+        <div>
+          <Button onClick={() => handleGoogleLogin(setUser)}>Log in with google</Button>
+          <Button onClick={() => handleGithubLogin(setUser)}>Log in with github</Button>
+          <Button onClick={() => handleLogOut(setUser)}>Log-out</Button>
+        </div>
+      </div>
+
+      <div className='py-2 my-10 border-2'>
         <div>
           <label htmlFor='id'>id</label>
-          <input id='id' onChange={(e) => setId(e.target.value)} />
+          <input className='mx-4 border-2' id='id' onChange={(e) => setId(e.target.value)} />
         </div>
         <div>
           <label htmlFor='content'>content</label>
-          <input id='content' onChange={(e) => setContent(e.target.value)} />
+          <input
+            className='mx-4 mb-4 border-2'
+            id='content'
+            onChange={(e) => setContent(e.target.value)}
+          />
         </div>
 
+        {/* Data Buttons */}
         <Button onClick={() => getData(setLists)}>새로 불러오기</Button>
         <Button onClick={() => addData(id, content)}>데이터 추가하기</Button>
         <Button onClick={() => deleteData(id)}>데이터 삭제</Button>
@@ -41,11 +57,10 @@ function App() {
 
       {lists.map((list, i) => {
         return (
-          <div key={i}>
+          <div className='p-5 my-2 bg-red-200 border-2' key={i}>
             <div>{`id : ${list.id}`}</div>
             <div>{`content : ${list.content}`}</div>
             <div>{`date : ${list.date}`}</div>
-            <br />
           </div>
         );
       })}
