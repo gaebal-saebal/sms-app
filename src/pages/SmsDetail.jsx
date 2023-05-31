50837e0337ae1b134ab98651e903f62ff4952988
rosenfence <rosenfence@gmail.com>
2023-06-01 01:46:05 +0900

    chore : 페이지 분류

---
+++ src/pages/SmsDetail.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SmsDetail = () => {
+  return <div></div>;
+};
+
+export default SmsDetail;
---
+++ src/pages/WriteSms.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WriteSms = () => {
+  return <div></div>;
+};
+
+export default WriteSms;
