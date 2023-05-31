9d9274b71ab4ec60360c88aec9dc094304105d1d
rosenfence <rosenfence@gmail.com>
2023-06-01 01:47:42 +0900

    chore : pages/index.js 추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,7 @@
+import FriendList from './FriendList';
+import Login from './Login';
 import SignUp from './SignUp';
+import SmsDetail from './SmsDetail';
+import WriteSms from './WriteSms';
 
-export { SignUp };
+export { SignUp, Login, FriendList, SmsDetail, WriteSms };
