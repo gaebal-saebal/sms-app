1abd3c62d8eee8215a23b90b024747b98dbc4751
js-ha <jshaha0911@gmail.com>
2023-06-13 23:58:56 +0900

    feat : 보낸사람 고유아이디 이메일주소로 변경

--- src/pages/SmsDetail.jsx
+++ src/pages/SmsDetail.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { getData } from '../function/db';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase/firebase';
 
 const SmsDetail = () => {
   const [lists, setLists] = useState([]);
   const [sms, setSms] = useState([]);
+  const [users, setUsers] = useState([]);
 
   let userId = window.sessionStorage.getItem('userId');
 
@@ -20,7 +23,7 @@ const SmsDetail = () => {
       return e.createdAt;
     });
     arr2.sort((a, b) => a - b);
-    console.log(arr2);
+
     // arr2 : 작성일자 순서대로 정렬된 createdAt만으로 구성된 배열
 
     const arr3 = new Array();
@@ -42,20 +45,38 @@ const SmsDetail = () => {
     getSms();
   }, [lists]);
 
-  console.log(sms);
+  useEffect(async () => {
+    const querySnapshot = await getDocs(collection(db, 'users'));
+    querySnapshot.forEach((doc) => {
+      // 가져온 모든 문서들을 확인
+      setUsers((prev) => [...prev, doc.data()]);
+    });
+  }, []);
+
+  function isName(senderId) {
+    for (let i = 0; i < users.length; i++) {
+      if (senderId === users[i].userId) {
+        return users[i].email;
+      }
+    }
+  }
+  console.log(users);
 
   return (
     <div>
       <div>리스트네요</div>
-      {sms.map((msg, i) => {
-        return (
-          <div key={i}>
-            <div>내용 : {msg.content}</div>
-            <div>받은시간 : {msg.date}</div>
-            <div>보낸사람 : {msg.senderId}</div>
-          </div>
-        );
-      })}
+
+      {sms.length > 0
+        ? sms.map((msg, i) => {
+            return (
+              <div key={i}>
+                <div>내용 : {msg.content}</div>
+                <div>받은시간 : {msg.date}</div>
+                <div>보낸사람 : {isName(msg.senderId)}</div>
+              </div>
+            );
+          })
+        : null}
     </div>
   );
 };
