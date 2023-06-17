c505d42c79ae381504c29727393930f5350cd21b
rosenfence <rosenfence@gmail.com>
2023-06-17 21:41:29 +0900

    feat : Home에 올릴 공개 문자메시지 작성 기능 추가  로그인 페이지로 이동 구현

--- src/pages/WriteSms.jsx
+++ src/pages/WriteSms.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { addData } from '../function/db';
 import { collection, getDocs } from 'firebase/firestore';
 import { db } from '../firebase/firebase';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, Link } from 'react-router-dom';
 
 const WriteSms = () => {
   const [content, setContent] = useState('');
@@ -43,21 +43,36 @@ const WriteSms = () => {
     setRecieverId(isName(reciever));
   }, [users]);
 
-  return (
-    <div className='page-content scrollbar-hidden'>
-      <div>{recieverId}님께 보낼 메세지</div>
-      <input onChange={(e) => handleChangeText(e)} placeholder='문자내용' />
+  if (userId) {
+    return (
+      <div className='page-content scrollbar-hidden'>
+        {recieverId !== 'main@gaesae.com' ? (
+          <div>{`${recieverId} 님께 보낼 메세지`}</div>
+        ) : (
+          <div>익명의 모두에게 보낼 메시지를 작성해주세요!</div>
+        )}
+        <input onChange={(e) => handleChangeText(e)} placeholder='문자내용' />
 
-      <button
-        onClick={() => {
-          addData(content, reciever, userId, null, 'sms');
-          navigate('/');
-        }}
-      >
-        보내기
-      </button>
-    </div>
-  );
+        <button
+          onClick={() => {
+            addData(content, reciever, userId, null, 'sms');
+            navigate('/');
+          }}
+        >
+          보내기
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <div className='page-content scrollbar-hidden'>
+        <div>모두에게 문자메시지를 보내기 위해서 로그인 해주세요!</div>
+        <Link className='p-2 text-white rounded-lg bg-main-pink' to='/login'>
+          로그인하러 가기
+        </Link>
+      </div>
+    );
+  }
 };
 
 export default WriteSms;
