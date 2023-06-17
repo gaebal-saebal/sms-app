8f4035b65f445eb671cbb8266cc603e561679d1b
rosenfence <rosenfence@gmail.com>
2023-06-17 21:40:22 +0900

    chore : SmsDetail Page TODO 주석 추가

--- src/pages/SmsDetail.jsx
+++ src/pages/SmsDetail.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import { useParams, useSearchParams } from 'react-router-dom';
 
 const SmsDetail = () => {
@@ -6,10 +7,16 @@ const SmsDetail = () => {
   const createdAt = queryString.get('createdAt'); // 리뷰 작성 시간
   const recieverId = useParams(); // 리뷰 받은 사람의 id
 
+  const [sms, setSms] = useState({});
+
   /*
   TODO:
   db - sms에서 createdAt과 recieverId를 사용해 일치하는 문자 메시지를 가져와 화면에 출력해주세요.
   새로운 함수는 function/db/getSmsDetail.js로 만들어서 사용해주세요.
+  새 함수인 getSmsDetail 함수는 params로 setSms와 createdAt, recieverId를 받아주세요.
+  새 함수 내에서는 1. db에서 sms 중 createdAt이 일치하는 document를 가져오고,
+  1. 에서 recieverId가 params로 받은 recieverId인 것을 filter해서,
+  setSms로 sms 상태에 객체 형식으로 넣어주세요.
   */
 
   return <div className='page-content scrollbar-hidden'>상세 문자메시지 페이지</div>;
