e44f63ce08a35162bec3ba9bb2e78b7a3b3fdc60
js-ha <jshaha0911@gmail.com>
2023-06-17 23:19:29 +0900

    design : 네브바 클릭시 폰트색상 변경 및 유지

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { handleLogOut } from '../function/auth';
 
 const Navbar = ({ isLogin, setIsLogin }) => {
+  const location = useLocation();
+  const urlName = location.pathname;
+
   return (
     <div className='absolute bottom-0 flex justify-between w-full h-16 px-10 align-center bg-main-blue'>
-      <Link to='/' className='h-full flex-center'>
+      <Link
+        to='/'
+        className={`h-full flex-center ${urlName === '/' ? 'text-white' : 'hover:text-white'}`}
+      >
         홈으로
       </Link>
-      <Link to='/friend' className='h-full flex-center'>
+      <Link
+        to='/friend'
+        className={`h-full flex-center ${
+          urlName === '/friend' ? 'text-white' : 'hover:text-white'
+        }`}
+      >
         친구목록
       </Link>
       {isLogin ? (
         <>
-          <Link to='/sms' className='h-full flex-center'>
+          <Link
+            to='/sms'
+            className={`h-full flex-center ${
+              urlName === '/sms' ? 'text-white' : 'hover:text-white'
+            }`}
+          >
             SMS
           </Link>
           <div
-            className='flex-center hover:cursor-pointer'
+            className='flex-center hover:cursor-pointer hover:text-white'
             onClick={() => handleLogOut(setIsLogin)}
           >
             로그아웃
@@ -25,10 +41,20 @@ const Navbar = ({ isLogin, setIsLogin }) => {
         </>
       ) : (
         <>
-          <Link to='/login' className='h-full flex-center'>
+          <Link
+            to='/login'
+            className={`h-full flex-center ${
+              urlName === '/login' ? 'text-white' : 'hover:text-white'
+            }`}
+          >
             로그인
           </Link>
-          <Link to='/signup' className='h-full flex-center'>
+          <Link
+            to='/signup'
+            className={`h-full flex-center ${
+              urlName === '/signup' ? 'text-white' : 'hover:text-white'
+            }`}
+          >
             회원가입
           </Link>
         </>
