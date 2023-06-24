b8633e95f3bfae486174286891039783fe591e38
rosenfence <rosenfence@gmail.com>
2023-06-25 01:54:10 +0900

    chore : TODO 주석 추가

--- src/function/auth/githubOauthLogin.js
+++ src/function/auth/githubOauthLogin.js
@@ -8,14 +8,14 @@ const handleGithubLogin = (setIsLogin) => {
       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
       const credential = GithubAuthProvider.credentialFromResult(result);
       const token = credential.accessToken;
-      console.log(token);
       // The signed-in user info.
       const user = result.user;
       const accessToken = user.accessToken;
       window.sessionStorage.setItem('accessToken', accessToken);
       setIsLogin(accessToken);
       // IdP data available using getAdditionalUserInfo(result)
-      // ...
+      /*TODO: addData(null, null, user.uid, user.email, 'users');
+      해서 로그인/회원가입 시 users에 회원정보 추가하기(google OAuth도 마찬가지)*/
     })
     .catch((error) => {
       console.log(error);
